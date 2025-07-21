1ce3dcb9caa38b1d273f87bcff89874ee1ddda6d
shobhit <shobhit@hmail.com>
2025-07-21 11:51:24 +0000

    small script change

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 echo "im working on it.................................. "
 sudo apt update -y  
-touch {file1..50 }
+touch /root/linux-git file linux{1..50}
 echo "EVERYTHING IS UNDER CONTROL"
 echo "WORK DONE"
